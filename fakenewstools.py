4c17a6fb9e231cdfc3bbdde7d99d01366d60d55b
Gilsiley Henrique Daru <gilsiley.daru@neogrid.com>
2021-12-08 14:42:37 -0300

    Alterações de Interface

--- fakenewstools.py
+++ fakenewstools.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import sqlite3
 import numpy as np
-
+import pickle
+import joblib
 
 def getrandomfake():
     with sqlite3.connect('fakenewsbr.db') as con:
@@ -17,5 +18,27 @@ def getfakedatabase():
     with sqlite3.connect('fakenewsbr.db') as con:
         fakes = pd.read_sql("Select * from tblFakeNews",con)
     return fakes.to_json(orient='records')
+
+def getmodelsdataframe():
+    with sqlite3.connect('fakenewsbr.db') as con:
+        df_models = pd.read_sql("Select * from tblModels",con)
+        return df_models
+
+def getmodelslistname(df, nm_model):
+    return df[nm_model].values
+
+def getmodelslistid(df, id_model):
+    return df[id_model].values
+
+def evaluatemodel(text, file_model):
+    #source https://realpython.com/python-import/
+    #import importlib
+    #Carrega o modelo    
+    #modelo = importlib.import_module('modelos/'+file_model)
+    print(f"**************Nome do Arquivo: {file_model} *********{__name__}****************")
+    model = joblib.load("modelos/"+file_model)
+    return model.predict(text)
+
+
 
 
